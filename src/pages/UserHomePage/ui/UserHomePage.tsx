5e6b4453c294d216b66625245da1387c92f18b9c
JavaScriptNoob <Timur.bobylev@yandex.ru>
2023-07-20 04:02:06 +0200

    updated user page

---
+++ src/pages/UserHomePage/ui/UserHomePage.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from "react";
+import { Autorization } from "widgets/ModalAutorization";
+
+export const UserHomePage  = () => {
+  return (
+    <div className="Main">
+      <div className="container">
+        <Autorization />
+      </div>
+
+    </div>
+  );
+
+
+};
+export default UserHomePage
