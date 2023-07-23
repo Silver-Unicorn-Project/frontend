05b28bd014efcd3ff9f285040425a7bdc6d438c4
JavaScriptNoob <Timur.bobylev@yandex.ru>
2023-07-23 18:42:58 +0200

    selectors

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,23 +1,34 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
 import { Swiper } from 'widgets/Swiper';
 import {CardItem} from "widgets/CardItem";
 import getProductsData from "pages/MainPage/actions";
-import {useDispatch} from "shared/lib/customHooks/customHooks";
+import {useDispatch, useSelector} from "shared/lib/customHooks/customHooks";
+import { RootState } from 'index';
 
 export const MainPage = () => {
   const dispatch = useDispatch();
+  const [loading, setLoading]= useState(true);
+  
+  const itemsData =  useSelector((state:RootState) => state.todos.items)
 console.log("I am here")
   useEffect(()=>{
       dispatch(getProductsData())
-
-
+      setLoading(false)
+    
   },[])
+
+  if(loading === false){
+    console.log(itemsData)
+  }
   return (
+
     <div className="Main">
       <div className="container">
         <Swiper />
       </div>
+      <CardItem/>
+     
       <CardItem/>
     </div>
   );
--- src/shared/lib/customHooks/customHooks.tsx
+++ src/shared/lib/customHooks/customHooks.tsx
@@ -1,4 +1,6 @@
+import { RootState } from 'index';
 import { TypedUseSelectorHook, useDispatch as dispatchHook, useSelector as selectorHook } from 'react-redux'
 import {AppDispatch} from "shared/lib/GeneralTypes/ReduxTypes";
 
 export const useDispatch = () => dispatchHook<AppDispatch>();
+export const useSelector: TypedUseSelectorHook<RootState> = selectorHook;
